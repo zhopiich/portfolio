fa85e23615fdaee18327401ad7b40236d028681e
zhopiich <zhoping9449@gmail.com>
2024-09-18 22:56:47 +0800

    adjust the layout of the home page

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer z-10 absolute bottom-0 h-8 text-center">
+    <span class="text-zinc-500 text-sm">Made by zhopiich © {{ year }}</span>
+  </div>
+</template>
+
+<script setup>
+const year = new Date().getFullYear();
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  width: calc(100% - calc(var(--width-slider) * var(--ratio)));
+
+  @media screen and (max-width: 767px) {
+    width: 100%;
+  }
+}
+</style>
