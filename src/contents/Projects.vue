6090134e6cf36d030ca5e446ee18da50a9ccb3c7
zhopiich <zhoping9449@gmail.com>
2026-05-18 22:34:12 +0800

    feat: expand projects showcase

--- src/contents/Projects.vue
+++ src/contents/Projects.vue
@@ -1,102 +1,62 @@
 <template>
   <div class="flex justify-center">
-    <div class="min-w-[312px] max-w-[420px] lg:max-w-[950px] flex flex-col">
-      <div
+    <div
+      class="min-w-[312px] max-w-[420px] lg:max-w-[950px] flex flex-col gap-20 lg:gap-24 py-4 lg:py-8"
+    >
+      <ProjectShowcase
         v-for="project in projects"
-        class="w-full grid grid-cols-1 lg:grid-cols-11 grid-rows-[repeat(2,min-content)] lg:grid-rows-1 gap-2 lg:gap-0"
-      >
-        <div
-          class="col-start-1 -col-end-1 lg:col-end-8 row-start-1 row-end-2 lg:-row-end-1 lg:pr-4"
-        >
-          <Carousel />
-        </div>
-
-        <div
-          class="col-start-1 lg:col-start-7 -col-end-1 row-start-2 lg:row-start-1 -row-end-1"
-        >
-          <div
-            class="lg:h-full grid grid-cols-1 lg:grid-cols-5 grid-rows-[min-content,_min-content,_auto] gap-4"
-          >
-            <div
-              class="col-start-1 lg:col-start-2 -col-end-1 row-start-1 row-end-2"
-            >
-              <h1 class="text-2xl font-bold text-slate-200">
-                {{ project.name }}
-              </h1>
-
-              <div class="mt-2.5 flex flex-wrap gap-x-2 gap-y-1.5">
-                <div
-                  v-for="tech in project.techs"
-                  class="px-3 py-1 bg-cyan-900 rounded-full flex shadow-x"
-                >
-                  <span class="text-sky-300">{{ tech }}</span>
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="col-start-1 -col-end-1 row-start-2 row-end-3 relative my-2"
-            >
-              <div
-                class="lg:p-5 lg:bg-cyan-950 lg:rounded lg:shadow-md shadow-slate-900"
-              >
-                <span class="text-blue-200 lg:text-blue-100 text-lg">{{
-                  project.desc
-                }}</span>
-              </div>
-            </div>
-
-            <div
-              class="col-start-1 lg:col-start-2 -col-end-1 -row-end-2 -row-start-1 flex justify-end items-end"
-            >
-              <div class="flex flex-wrap gap-3">
-                <LinkButton
-                  :icon="PajamasPlay"
-                  name="Demo"
-                  :url="project.urls.demo"
-                  color="#4F5676"
-                  :isColorFull="true"
-                  textColor="#FFF4D3"
-                />
-
-                <LinkButton
-                  :icon="MdiGithub"
-                  name="Github Repo"
-                  :url="project.urls.repo"
-                  color="#4F5676"
-                  textColor="#FFF4D3"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        :key="project.name"
+        :project="project"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import PajamasPlay from "~icons/pajamas/play";
-import MdiGithub from "~icons/mdi/github";
+import ProjectShowcase from "../components/ProjectShowcase.vue";
 
-import LinkButton from "../components/LinkButton.vue";
-import Carousel from "../components/Carousel.vue";
+import instaparodyHome from "../../public/home.png";
+import instaparodyMobile from "../../public/mobiless.png";
+import instaparodyPostDetails from "../../public/post_details.png";
+import instaparodyMessages from "../../public/messages.png";
+import instaparodyProfile from "../../public/profile.png";
+import instaparodySignup from "../../public/signup.png";
+import tanstackDemo from "../../public/tanstack-demo.png";
 
 const projects = [
   {
     name: "Instaparody",
     techs: ["Vue 3", "Vue Router", "Pinia", "Tailwind CSS", "Firebase", "Vite"],
     desc: "An Instagram clone integrated with a clone of Twitter's direct messages",
+    images: [
+      instaparodyHome,
+      instaparodyMobile,
+      instaparodyPostDetails,
+      instaparodyMessages,
+      instaparodyProfile,
+      instaparodySignup,
+    ],
     urls: {
       demo: "https://instaparody.vercel.app",
       repo: "https://github.com/zhopiich/instaparody",
     },
   },
+  {
+    name: "TanStack Demo",
+    techs: [
+      "Vue 3",
+      "TypeScript",
+      "TanStack Query",
+      "TanStack Table",
+      "Zod",
+      "MSW",
+    ],
+    desc: "A simulated content management system demonstrating the integration of TanStack Query, Zod, and OpenAPI schemas.",
+    images: [tanstackDemo],
+    urls: {
+      demo: "https://zhopiich.github.io/tanstack-demo/",
+      repo: "https://github.com/zhopiich/tanstack-demo",
+    },
+  },
 ];
 </script>
-
-<style lang="scss" scoped>
-.shadow-x {
-  box-shadow: rgba(60, 60, 60, 0.4) 0px 1px 10px;
-}
-</style>
